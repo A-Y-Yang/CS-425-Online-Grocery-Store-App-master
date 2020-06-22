de9a104d3f5b05b56b6a03aef622bda3dcc23a01
AK Ho <kho5@hawk.iit.edu>
2020-06-22 17:15:00 -0500

    Modified to webapp-v4.3

--- Application/shop/product/routes.py
+++ Application/shop/product/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, flash, session, current_app
 from shop import app, db, photos, login_manager
 from flask_login import login_required, logout_user, current_user
-from shop.admin.models import Category, Product, Orders, OrderItem, Owns, Customer, Warehouse, Availability
+from shop.admin.models import Category, Product, Orders, OrderItem, Owns, Customer, Warehouse, Availability, Staff
 from shop.customer.forms import Checkout
 from .forms import Addproduct
 from decimal import Decimal
@@ -34,66 +34,82 @@ def updatecat(id):
         return redirect(url_for('categories'))
     return render_template('product/updatecat.html', title = "Update Category Page", updatecat = updatecat)
 
-@app.route('/addproduct', methods=['GET', 'POST'])
-def addproduct():
+@app.route('/addproduct/<int:id>', methods=['GET', 'POST'])
+def addproduct(id):
     if 'email' not in session:
         flash(f'Please login first','danger')
         return redirect(url_for('login'))
-    categories = Category.query.all()
-    form = Addproduct(request.form)
-    if request.method == 'POST':
-        image_1 = photos.save(request.files.get('image'))
-        product = Product(product_name = form.product_name.data, price = form.price.data,
-                    size = form.size.data, add_info = form.add_info.data, 
-                    category_id = request.form.get('category'), image = image_1)
-        db.session.add(product)
-        db.session.commit()
-        flash(f'{form.product_name.data} is added to your database.', 'success')
-        return redirect(url_for('admin'))
-    return render_template('product/addproduct.html', title = "Add Product Page", form = form, categories = categories)
+    try: 
+        staff = Staff.query.get_or_404(id)
+        categories = Category.query.all()
+        form = Addproduct(request.form)
+        if request.method == 'POST':
+            image_1 = photos.save(request.files.get('image'))
+            product = Product(product_name = form.product_name.data, price = form.price.data,
+                        size = form.size.data, add_info = form.add_info.data, 
+                        category_id = request.form.get('category'), image = image_1)
+            db.session.add(product)
+            db.session.commit()
+            flash(f'{form.product_name.data} is added to your database.', 'success')
+            return redirect(url_for('admin', id = id))
+    except Exception as e:
+        print(e)
+        flash(f'Cannot add the product.','danger')
+        return redirect(url_for('admin', id = id))
+    return render_template('product/addproduct.html', title = "Add Product Page", staff = staff, form = form, categories = categories)
 
-@app.route('/updateproduct/<int:id>', methods=['GET', 'POST'])
-def updateproduct(id):
+@app.route('/updateproduct/<int:id>/<int:product_id>', methods=['GET', 'POST'])
+def updateproduct(id, product_id):
     if 'email' not in session:
         flash(f'Please login first','danger')
         return redirect(url_for('customer_login'))
-    categories = Category.query.all()
-    product = Product.query.get_or_404(id)
-    form = Addproduct(request.form)
-    category = request.form.get('category')
-    if request.method == 'POST':
-        product.product_name = form.product_name.data
-        product.price = form.price.data
-        product.size = form.size.data
-        product.add_info = form.add_info.data
-        product.category_id = category
-        db.session.commit()
-        flash(f'Your product {form.product_name.data} has been updated.', 'success')
-        return redirect(url_for('admin'))
-    form.product_name.data = product.product_name
-    form.price.data = product.price
-    form.size.data = product.size
-    form.add_info.data = product.add_info
-    category = product.category_id
-    return render_template('product/updateproduct.html', title = "Update Product Page", form = form, categories = categories, product = product)
+    try:
+        categories = Category.query.all()
+        staff = Staff.query.get_or_404(id)
+        product = Product.query.get_or_404(product_id)
+        form = Addproduct(request.form)
+        category = request.form.get('category')
+        if request.method == 'POST':
+            product.product_name = form.product_name.data
+            product.price = form.price.data
+            product.size = form.size.data
+            product.add_info = form.add_info.data
+            product.category_id = category
+            db.session.commit()
+            flash(f'Your product {form.product_name.data} has been updated.', 'success')
+            return redirect(url_for('admin', id = id))
+        form.product_name.data = product.product_name
+        form.price.data = product.price
+        form.size.data = product.size
+        form.add_info.data = product.add_info
+        category = product.category_id
+    except Exception as e:
+        print(e)
+        flash(f'Cannot update the product.','danger')
+        return redirect(url_for('admin', id = id))
+    return render_template('product/updateproduct.html', title = "Update Product Page", staff = staff, form = form, categories = categories, product = product)
 
-@app.route('/deleteproduct/<int:id>', methods=["POST"])
-def deleteproduct(id):
+@app.route('/deleteproduct/<int:id>/<int:product_id>', methods=["POST"])
+def deleteproduct(id, product_id):
     if 'email' not in session:
         flash(f'Please login first','danger')
         return redirect(url_for('customer_login'))
-    product = Product.query.get_or_404(id)
-    if request.method == "POST":
-        try:
-            os.unlink(os.path.join(current_app.root_path, "static/images" + product.image))
-        except Exception as e:
-            print(e)
-        db.session.delete(product)
-        db.session.commit()
-        flash(f'Your product {product.product_name} was deleted.', 'success')
-        return redirect(url_for('admin'))
-    flash(f'Cannot delete the product.','danger')
-    return render_template(url_for('admin'))
+    try:
+        staff = Staff.query.get_or_404(id)
+        product = Product.query.get_or_404(product_id)
+        if request.method == "POST":
+            try:
+                os.unlink(os.path.join(current_app.root_path, "static/images" + product.image))
+            except Exception as e:
+                print(e)
+            db.session.delete(product)
+            db.session.commit()
+            flash(f'Your product {product.product_name} was deleted.', 'success')
+            return redirect(url_for('admin', id = id))
+    except Exception as e:
+        print(e)
+        flash(f'Cannot delete the product.','danger')
+        return redirect(url_for('admin', id = id))
 
 @app.route('/product_details/<int:id>', methods=['GET', 'POST'])
 def product_details(id):
@@ -102,13 +118,6 @@ def product_details(id):
         return redirect(url_for('login'))
     product = Product.query.get_or_404(id)
     return render_template('product/product_details.html', product = product)
-
-#@app.route('/getorder')
-#def getOrder():
-#    if current_user.is_authenticated:
-#        customer_id = current_user.id
-#        try:
-#            order = Order(customer_id= customer_id, orders=session['Shoppingcart'])
 
 @app.route('/addorder/<int:id>', methods=['GET', 'POST'])
 def addorder(id):
@@ -130,7 +139,7 @@ def addorder(id):
             db.session.add(orderitem)
             customer = Customer.query.filter_by(customer_id = id).first()
             warehouse = Warehouse.query.filter_by(a_state = customer.da_state).first()
-            reduceavailability = Availability.query.filter_by(product_id = int(key), warehouse_id = warehouse.warehouse_id).first()
+            reduceavailability = Availability.query.filter_by(product_id = int(key)).filter_by(warehouse_id = warehouse.warehouse_id).first()
             if (reduceavailability.item_quantity > int(orderitem.quantity)):
                 reduceavailability.item_quantity -= int(orderitem.quantity)
                 db.session.add(orderitem)
